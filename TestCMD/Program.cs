30e68b29ab880483a2fb1a4704ef1f0b6e716312
Petko Petkov <Petko_Petkov@abv.bg>
2017-06-18 18:09:50 +0300

    C1 test

---
+++ TestCMD/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 18.06.2017 г.
+ * Time: 18:03
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TestCMD
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
