7dc1d45a47727073e8252ea4d0a4a21a7a60c901
PetkoPetkov <petko_petkov@abv.bg>
2017-06-19 11:24:20 +0300

    c7

---
+++ Restart/MainForm.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Petko
+ * Date: 05.06.2017
+ * Time: 17:08
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Restart
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			Shutdown.Shut();
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			Shutdown.Restart();
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			Shutdown.LogOff();
+		}
+
+	}
+}
---
+++ Restart/Restart.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Petko
+ * Date: 05.06.2017
+ * Time: 17:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+// The following code will execute the shutdown command from the shell:
+
+using System.Diagnostics;
+
+static class Shutdown
+{
+    /// <summary>
+    /// Windows restart
+    /// </summary>
+    public static void Restart()
+    {
+        StartShutDown("-f -r -t 5", " -c \"Computer will Restart in 5 seconds. !!!\"");
+    }
+
+    /// <summary>
+    /// Log off.
+    /// </summary>
+    public static void LogOff()
+    {
+        StartShutDown("-l");
+    }
+
+    /// <summary>
+    ///  Shutting Down Windows 
+    /// </summary>
+    public static void Shut()
+    {
+        StartShutDown("-f -s -t 5", " -c \"Computer will shut down in 5 seconds. !!!\"");
+    }
+
+    private static void StartShutDown(string param,string text = "")
+    {
+        var proc = new ProcessStartInfo();
+        proc.FileName = "cmd";
+        proc.WindowStyle = ProcessWindowStyle.Hidden;
+        proc.Arguments = "/C shutdown " + param + text;
+        Process.Start(proc);
+    }
+}
